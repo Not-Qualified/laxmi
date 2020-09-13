edf050a92888f700d9dcb206b3b9a5cee901a278
Not-Qualified <Dharmesh.srk@gmail.com>
2020-09-13 21:41:44 +0530

    Updating Sell Module

--- sell/models.py
+++ sell/models.py
@@ -1,21 +1,12 @@
 from django.db import models
-from django.db.models.constraints import UniqueConstraint
 from django.core.validators import MinValueValidator
 from person.models import Person
 from stock.models import Stock
+
 # Create your models here.
-class SellProductList(models.Model):
-	sell_product = models.ForeignKey(Stock, on_delete=models.PROTECT)
-	sell_product_quantity = models.IntegerField(default=1,
-												validators=[MinValueValidator(limit_value=1)])
-
-
 class Sell(models.Model):
 	person = models.ForeignKey(Person, on_delete=models.PROTECT)
-	sell_product_list = models.ForeignKey(SellProductList, on_delete=models.PROTECT, )
-
-
-	class Meta:
-		db_table = "sell"
-		constraints = [models.UniqueConstraint(fields=["person", "sell_product_list"], 
-												name="person_sell_product"), ]
+	sell_product = models.ForeignKey(Stock, on_delete=models.PROTECT)
+	sell_product_quantity = models.IntegerField(default=1, 
+												validators=[MinValueValidator(limit_value=0)])
+	date_added = models.DateTimeField(auto_now_add=True)
